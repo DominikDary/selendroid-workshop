01a3cd0103b8d6125f25c21beaac0bd91d09f887
Dominik Dary <ddary@acm.org>
2015-04-27 06:13:51 +0200

    adding result of first session.

--- src/test/java/de/dary/workshop/selendroid/FirstSelendroidTest.java
+++ src/test/java/de/dary/workshop/selendroid/FirstSelendroidTest.java
@@ -27,14 +27,45 @@ public class FirstSelendroidTest {
   private static WebDriver driver = null;
 
   @Test
-  public void shouldBeAbleToEnterText() throws Exception {
+  public void shouldRegisterNewAccount() throws Exception {
     SelendroidCapabilities caps =
         new SelendroidCapabilities("io.selendroid.testapp:0.16.0-SNAPSHOT");
 
     driver = new SelendroidDriver(caps);
-    WebElement inputField = driver.findElement(By.id("my_text_field"));
-    inputField.sendKeys("Selendroid");
-    Assert.assertEquals("Selendroid", inputField.getText());
+    driver.get("and-activity://io.selendroid.testapp.RegisterUserActivity");
+
+    WebElement username = driver.findElement(By.id("inputUsername"));
+    String userNameValue = "U$erNAme";
+    username.sendKeys(userNameValue);
+
+    String email = "me@myserver.com";
+    driver.findElement(By.name("email of the customer")).sendKeys(email);
+
+    String password = "myLittleSecret";
+    driver.findElement(By.id("inputPassword")).sendKeys(password);
+
+    WebElement nameInput = driver.findElement(By.xpath("//EditText[@id='inputName']"));
+    Assert.assertEquals(nameInput.getText(), "Mr. Burns");
+    nameInput.clear();
+    String name = "Romanian Tester";
+    nameInput.sendKeys(name);
+
+    driver.findElement(By.tagName("Spinner")).click();
+    String programmingLanguage = "Python";
+    driver.findElement(By.linkText(programmingLanguage)).click();
+
+    driver.findElement(By.className("android.widget.CheckBox")).click();
+
+    driver.findElement(By.linkText("Register User (verify)")).click();
+    Assert.assertEquals(driver.getCurrentUrl(), "and-activity://VerifyUserActivity");
+
+    Assert.assertEquals(driver.findElement(By.id("label_username_data")).getText(), userNameValue);
+    Assert.assertEquals(driver.findElement(By.id("label_email_data")).getText(), email);
+    Assert.assertEquals(driver.findElement(By.id("label_password_data")).getText(), password);
+    Assert.assertEquals(driver.findElement(By.id("label_name_data")).getText(), name);
+    Assert.assertEquals(driver.findElement(By.id("label_preferedProgrammingLanguage_data"))
+        .getText(), programmingLanguage);
+    Assert.assertEquals(driver.findElement(By.id("label_acceptAdds_data")).getText(), "true");
   }
 
   @BeforeClass
